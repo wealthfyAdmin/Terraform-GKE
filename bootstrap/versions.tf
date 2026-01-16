70f0fc803fb3c0aff42c1630e7d05e4b37dffc1a
wealthfyAdmin <161595953+wealthfyAdmin@users.noreply.github.com>
2026-01-16 11:51:07 +0000

    pinned exact versions in the bootstrap

--- bootstrap/versions.tf
+++ bootstrap/versions.tf
@@ -3,17 +3,17 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 7.16"
+      version = "7.16.0"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 3.0"
+      version = "3.0.1"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.1"
+      version = "3.1.1"
     }
   }
 }
