dcf2a83b417725a7b77e314380b985f446ab835e
wealthfyAdmin <161595953+wealthfyAdmin@users.noreply.github.com>
2026-01-16 09:46:38 +0000

    added outputs needed for the bootstrap phase

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,6 +2,15 @@ output "cluster_name" {
   value = google_container_cluster.autopilot.name
 }
 
-output "endpoint" {
+output "cluster_region" {
+  value = google_container_cluster.autopilot.location
+}
+
+output "cluster_endpoint" {
   value = google_container_cluster.autopilot.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value     = google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate
+  sensitive = true
 }
